e0cb13bc88175f7730217130d40bb079f2d98d75
ladislao <ladislao.ramirez@gmail.com>
2016-12-24 17:20:07 -0700

    Collapsing the navigator bar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Project Flyer</title>
 	<link rel="stylesheet" href="/css/app.css">
   <link rel="stylesheet" href="/css/libs.css">
