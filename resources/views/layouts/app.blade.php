bccf46985bb0b0273258ffd187a551e8f51d5d24
ladislao <ladislao.ramirez@gmail.com>
2016-12-24 18:15:06 -0700

    Bootstrap js added to app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,6 +84,8 @@
     @yield('content')
     
     <!-- Scripts -->
+  <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     {{-- <script src="/js/app.js"></script> --}}
 </body>
 </html>
